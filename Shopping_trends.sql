2b02f3bfac052f7aa6a7f83f5bea386ef7ae8536
abduaslam <abduaslam098@gmail.com>
2026-04-03 11:47:51 +0200

    data cleaning and EDA

--- Shopping_trends.sql
+++ Shopping_trends.sql
@@ -1,1 +1,12 @@
-CREATE DATABASE TransectionTrends
+CREATE DATABASE TransectionTrends ;
+
+SELECT * FROM transectiontrends.shopping_trends;
+
+
+SELECT COUNT(`Customer ID`)
+FROM  transectiontrends.shopping_trends;
+
+-- Customer Who spend more than $50 
+	SELECT  `Customer ID`, count(`Purchase Amount (USD)`) 
+   FROM  transectiontrends.shopping_trends
+   WHERE `Purchase Amount (USD)`  >50
