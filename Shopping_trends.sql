5b4b7887be15d617974cce24c9f102ca530f1fd3
abduaslam <abduaslam098@gmail.com>
2026-04-05 17:01:49 +0200

    Run Queries to answer Business Questions from shopping trends

--- Shopping_trends.sql
+++ Shopping_trends.sql
@@ -12,4 +12,14 @@ WHERE  `Purchase Amount (USD)` > 50;
 
 -- Purchases in Winter
 SELECT * FROM transectiontrends.shopping_trends
-WHERE Season ="Winter"
+WHERE Season ="Winter" ;
+
+-- Total revenue
+SELECT SUM(`Purchase Amount (USD)`) as Total_revenue 
+FROM transectiontrends.shopping_trends ;
+
+-- Average purchase amount
+SELECT avg(`Purchase Amount (USD)`) as Avrage_amount
+FROM transectiontrends.shopping_trends ;
+
+
