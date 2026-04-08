c85588a8c53e93d10b31fd9dad99049db3cafb69
abduaslam <abduaslam098@gmail.com>
2026-04-08 11:45:48 +0200

    Run Queries to answer Business Questions from shopping trends

--- Shopping_trends.sql
+++ Shopping_trends.sql
@@ -32,6 +32,31 @@ ORDER BY Total_sales desc;
 SELECT `Item Purchased`, avg(`Review Rating`) as avg_rating
 FROM transectiontrends.shopping_trends 
 GROUP BY `Item Purchased`
-ORDER BY avg_rating DESC
+ORDER BY avg_rating DESC ;
+
+-- Which payment method is most used?
+SELECT `Payment Method`, COUNT(*) as most_used
+FROM transectiontrends.shopping_trends 
+GROUP BY `Payment Method`
+ORDER BY most_used DESC ;
+
+-- Do discounts increase purchases?
+SELECT `Discount Applied` , AVG(`Purchase Amount (USD)`) as Increase_purchease
+FROM transectiontrends.shopping_trends 
+GROUP BY  `Discount Applied`;
+
+-- Top 5 customers by spending
+SELECT `Customer ID`,sum(`Purchase Amount (USD)`) as top_spending
+FROM transectiontrends.shopping_trends 
+GROUP BY `Customer ID`
+ORDER BY top_spending DESC
+LIMIT 5 ;
+
+-- Customers with subscriptions vs without
+SELECT `Subscription Status`, COUNT(*) as Total_customers
+FROM transectiontrends.shopping_trends 
+GROUP BY `Subscription Status`
+
+
 
 
