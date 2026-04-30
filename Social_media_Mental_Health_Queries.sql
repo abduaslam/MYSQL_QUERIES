8dae7e39d7c547b041ea7205914d3dae5d145e88
abduaslam <abduaslam098@gmail.com>
2026-04-30 11:03:15 +0200

    run queries in my dataset

---
+++ Social_media_Mental_Health_Queries.sql
@@ -0,0 +1,8 @@
+
+SELECT *
+FROM MentalHealth.Teen_Mental_Health_Dataset
+
+-- What is the average daily social media usage by age group?
+-- Which platform is used the most (Instagram, TikTok, Both)?
+-- Do females or males spend more time on social media?
+-- What percentage of users spend more than 5 hours daily?
