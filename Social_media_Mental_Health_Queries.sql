05fe6ca82962045e6c015536de01fc3cf0816dc8
abduaslam <abduaslam098@gmail.com>
2026-05-01 21:53:45 +0200

    Social media Busniss questions

--- Social_media_Mental_Health_Queries.sql
+++ Social_media_Mental_Health_Queries.sql
@@ -3,6 +3,10 @@ SELECT *
 FROM MentalHealth.Teen_Mental_Health_Dataset
 
 -- What is the average daily social media usage by age group?
+SELECT age, AVG(daily_social_media_hours) AS avg_usage
+FROM MentalHealth.Teen_Mental_Health_Dataset
+GROUP BY age
+ORDER BY age DESC ;
 
 -- Which platform is used the most (Instagram, TikTok, Both)?
 SELECT platform_usage ,AVG(daily_social_media_hours) AS most_used
@@ -11,12 +15,17 @@ GROUP BY platform_usage
 ORDER BY most_used DESC
 
 -- Do females or males spend more time on social media?
-SELECT gender, SUM(daily_social_media_hours)AS more_spend
+SELECT gender, AVG(daily_social_media_hours) AS avg_usage
 FROM MentalHealth.Teen_Mental_Health_Dataset
 GROUP BY gender
-ORDER BY more_spend DESC 
+ORDER BY avg_usage DESC;
 
 -- What percentage of users spend more than 5 hours daily?
-SELECT AVG(daily_social_media_hours) as more_than5_hours
-FROM MentalHealth.Teen_Mental_Health_Dataset
-WHERE daily_social_media_hours >5
+SELECT 
+  (COUNT(CASE WHEN daily_social_media_hours > 5 THEN 1 END) * 100.0 / COUNT(*)) AS percentage_above_5
+FROM MentalHealth.Teen_Mental_Health_Dataset;
+
+-- Does sleep hours decrease with higher social media usage?
+SELECT 
+-- How does screen time before sleep affect sleep hours?
+-- Do teens with low physical activity have higher stress?
