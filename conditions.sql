808ac6aa3947045bfab081d5608ba16f5d027eab
abduaslam <abduaslam098@gmail.com>
2026-05-18 15:10:34 +0200

    Mysql conditons

---
+++ conditions.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE SQL_Queries ;
+
+-- Find all orders where the Category = 'Furniture' AND Profit > 100.
+SELECT * FROM SQL_Queries.Sample_Superstore
+WHERE Category='Furniture' AND Profit>100 ;
