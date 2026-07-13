af831ce35c6f98c021e2e342712f6175f7f19ecd
abduaslam <abduaslam098@gmail.com>
2026-07-13 13:21:05 +0200

    Practice basic queries  in mysql database

--- ALX/Dta.sql
+++ ALX/Dta.sql
@@ -49,6 +49,49 @@ VALUES
 ('John Smith', 'john.smith@gmail.com', '1995-04-15'),
 ('Maria Johnson', 'maria.johnson@gmail.com', '1998-09-22');
 
+INSERT INTO customers (full_name, email_address, date_of_birth)
+VALUES
+('Ahmed Ali', 'ahmed.ali@gmail.com', '1995-06-12'),
+('Sara Mohamed', 'sara.mohamed@gmail.com', '1998-03-25'),
+('John Smith', 'john.smith@gmail.com', '1992-11-08'),
+('Maria Johnson', 'maria.johnson@gmail.com', '2000-01-15'),
+('David Brown', 'david.brown@gmail.com', '1996-09-30'),
+('Omar Hassan', 'omar.hassan@gmail.com', '1994-07-20'),
+('Emily Wilson', 'emily.wilson@gmail.com', '1997-12-05');
+
+
+-- Insert values into bikes table
+INSERT INTO bikes (brand, type, daily_rental_rate, availability_status)
+VALUES
+('Trek', 'Mountain Bike', 25.00, 'Available'),
+('Giant', 'Road Bike', 30.00, 'Rented'),
+('Scott', 'Hybrid Bike', 20.00, 'Available'),
+('Cannondale', 'Electric Bike', 45.00, 'Maintenance'),
+('Specialized', 'Mountain Bike', 35.00, 'Available');
+
+-- insert new data into rentals table
+INSERT INTO rentals 
+(customer_id, bike_id, start_date, expected_return_date, actual_return_date, total_amount_charged)
+VALUES
+(1, 2, '2026-07-01', '2026-07-03', '2026-07-03', 90.00),
+(2, 1, '2026-07-02', '2026-07-05', '2026-07-05', 75.00),
+(3, 5, '2026-07-04', '2026-07-06', NULL, NULL),
+(4, 3, '2026-07-05', '2026-07-07', '2026-07-07', 60.00),
+(5, 4, '2026-07-06', '2026-07-08', NULL, NULL),
+(6, 2, '2026-07-08', '2026-07-10', '2026-07-10', 60.00),
+(7, 1, '2026-07-09', '2026-07-11', '2026-07-11', 50.00);
+
+-- Update rental record with rental_id = 3
+UPDATE rentals
+SET 
+    actual_return_date = CURDATE(),
+    total_amount_charged = 37.50
+WHERE rental_id = 3;
+
+--
+DELETE FROM rentals
+WHERE customer_id = 7;
+
 
 
 
