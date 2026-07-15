b8b0b3495cbde57d034d2c981687e03994552efc
abduaslam <abduaslam098@gmail.com>
2026-07-15 17:39:05 +0200

    Access_to_Basic_Services

---
+++ ALX/Access_to_Basic_Services.sql
@@ -0,0 +1,21 @@
+ Use united_nations ;
+
+-- Create a Summary Statistic Report in SQL
+SELECT 
+    Region,
+    MIN(Pct_managed_drinking_water_services) AS Min_managed_drinking_water_services,
+    MAX(Pct_managed_drinking_water_services) AS Max_Pct_managed_drinking_water_services,
+    COUNT(DISTINCT Country_name) AS numbers_of_countries,
+    round(SUM(Est_population_in_millions),2) AS Est_total_population_in_millions
+FROM Access_to_Basic_Services
+GROUP BY Region
+ORDER BY Est_total_population_in_millions DESC ;
+
+
+
+
+
+
+
+
+
