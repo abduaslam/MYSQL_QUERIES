f205cb89b05328da2606c6cf23ab77c706a048c7
abduaslam <abduaslam098@gmail.com>
2026-07-20 11:08:42 +0200

    cleaning and standarize data using String and date Functions

--- ALX/Chinook_SQL_String_ Functions.sql
+++ ALX/Chinook_SQL_String_ Functions.sql
@@ -1,4 +1,5 @@
---  Data Cleaning and Standardization
+           
+           -- Part 1 Data Cleaning and Standardization
 /*
 Task 1: Customer Email Clean-Up
 The marketing team has noticed some customer email addresses have leading or trailing spaces. 
@@ -37,4 +38,63 @@ SELECT FirstName,LastName,Address,Phone,
        LastName," ",
        SUBSTRING(Address,1,8),":",
        Phone) AS EmployeeContact 
-FROM Customer;       
+FROM Customer;   
+
+
+                -- Part 2 Data Transformation and Reporting
+
+/*
+Task 4: Extracting Customer's City Code
+The logistics team wants to analyze customer distribution. 
+They need a report showing the CustomerId, FirstName, LastName, and a new column called CityCode. 
+The CityCode should be the first three characters of the City column, in lowercase.
+Query: Use a substring function and a case conversion function to extract and format the city code.
+*/    
+SELECT CustomerId,
+      FirstName,
+      LastName,
+      SUBSTRING(LOWER(City),1,3) AS CityCode
+FROM Customer;
+
+/*
+Task 5: Invoice Date Formatting
+The accounting department needs a report of all invoices.
+ They want the InvoiceDate to be formatted as a string in the YYYY-MM-DD format. 
+The report should include the InvoiceId, CustomerId, and the new formatted date column.
+Query: Use a date formatting function to convert the InvoiceDate into YYYY-MM-DD format.
+*/
+SELECT * FROM Invoice;
+
+SELECT InvoiceId,CustomerId,InvoiceDate ,DATE_FORMAT(InvoiceDate , "%Y-%m-%d" )AS FormattedInvoiceDate
+FROM Invoice;
+
+/*
+Task 6: Calculating a 'Customer ID Hash'
+
+For internal system checks, the IT department needs a unique, standardized identifier for each customer. 
+Create a report with CustomerId and a new column called CustomerID_Hash. 
+This hash should be a string created by concatenating the first two letters of the FirstName and the first two letters of the LastName, both in lowercase.
+Query: Use substring and case conversion functions to extract the first two letters of the FirstName and LastName, convert them to lowercase, and concatenate them.
+*/
+SELECT FirstName,
+      LastName ,
+LOWER(CONCAT(SUBSTRING( FirstName ,1,2),'_',
+       SUBSTRING(LastName,1,2)))   AS CustomerID_Hash
+FROM Customer ;
+
+-- Task 7: Customer Data Type Conversion
+
+
+                  -- Part 3: Advanced Reporting and Auditing
+                  
+                  
+/*Task 7: Customer Data Type Conversion
+The reporting tool used by management expects the InvoiceId to be a string. 
+Create a query that shows the InvoiceId, but with its data type explicitly converted to CHAR (or VARCHAR) using CAST(). Also, include the CustomerId and Total for each invoice.
+
+*/
+SELECT
+    InvoiceId,
+    CAST(InvoiceId AS CHAR(20)) AS InvoiceId_Text
+FROM Invoice;
+
